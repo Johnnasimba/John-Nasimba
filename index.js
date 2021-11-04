b11e0cc17b0bb40077c7f38e131de4a30dacc605
JOHN NASIMBA <nasimba4john@gmail.com>
2021-11-04 09:33:24 +0200

    setting up heroku

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ app.use((req, res, next) => {
     res.header({"Access-Control-Allow-Origin": "*"});
     next();
   }) 
+  if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build','index.html'))
@@ -75,6 +78,7 @@ app.post('/send', (req, res) => {
     }
     main().catch(console.error);
 })
+
 
 
 const PORT=process.env.PORT||8000;
